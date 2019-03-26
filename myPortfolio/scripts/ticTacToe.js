2ad534427169515388d661498f846a5abd80a224
TristanHoladay <tholaday777@gmail.com>
2019-03-25 19:50:15 -0600

    tic tac toe with JS

---
+++ myPortfolio/scripts/ticTacToe.js
@@ -0,0 +1,25 @@
+// clears board
+
+// create markers x and o
+
+var gameMarker = "X"
+var gameMarker = "O"
+
+// changes marker
+
+// makes player default marker
+
+// chooses which marker goes first
+
+//places marker on location onclick
+
+function placeMark(elementId) {
+    var box = document.getElementById(elementId);
+    box.innerHTML = 
+}
+
+//keeps track of how many squares are marked
+
+//declares win or lose by getting 3 in a row
+
+//
