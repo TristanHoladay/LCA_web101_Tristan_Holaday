0521e0e27894b151dc5ae171bc7e088680d474db
TristanHoladay <tholaday777@gmail.com>
2019-04-07 14:43:06 -0600

    cicker game in progress

--- myPortfolio/scripts/dashboard.js
+++ myPortfolio/scripts/dashboard.js
@@ -1,3 +1,5 @@
+
+// To Do List
 $(function() {
 
     let $itemField = $('#itemField');
@@ -28,6 +30,7 @@ $(function() {
 
 });
 
+// Geolocation API
 $(document).ready(function($) {
     
     var $locdisplay = $('.coordinates')
@@ -51,6 +54,7 @@ function showPosition(position) {
     $('.coordinates').addClass('visible');
 };
 
+// Refresh the Coordinates
 $(function() {
     var $refresh = $('#refresh');
     var $newloc = $("<div></div>").text("New Location: ");
@@ -62,4 +66,115 @@ $(function() {
         });
 });
 
+// Google Maps
+$(function() {
+    var $mapBtn = $('#mapBtn');
 
+    $mapBtn.on('click', function(e) {
+        e.preventDefault();
+
+        
+        var $mapLatLng = new google.maps.Latlng(lat, lng);
+
+        var mapOptions = {
+            zoom: 15,
+            mapTypeControl: false,
+            center: $mapLatLng,
+        };
+
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+        var mapMarker = new google.maps.Marker({
+            position: mapLatLng,
+            map: map,
+            title: 'Your broswer/device places you here',
+        });
+
+        $(window).resize(function() {
+            google.maps.event.trigger(map, 'resize');
+            map.panTo(mapLatLng);
+        });
+
+    });
+});
+
+
+// Clicker Game
+$(function() {
+
+    var $gameBtn = $('#gameBtn');
+    var $arr = $('.squares').toArray();
+    var $icon = $('#robot');
+    var $health = 100;
+    var $lvlBtn = $('#upgrade');
+    var $lvl = 0;
+    var $damage = (5 + ($lvl*2));
+    var $money = 0;
+    
+
+    // when the play button is clicked, execute these functions
+    $gameBtn.on('click', function(e) {
+        e.preventDefault();
+
+            // Appends Icons to Grid Squares at Set Interval
+            setInterval(function() {
+                randomId();
+            }, 3000);
+
+            $('.money').text("Money = " + $money);
+
+             //Increases Money
+            $("[id^=square]").click(function () {
+
+                
+                    var $currentHealth =  ($health -= $damage);
+                    $money += 2;
+                    console.log($damage);
+                
+
+                if ($currentHealth <= 0) {
+                    $( this ).remove($icon);
+                    $money += 10;
+                };
+                
+                if ($money === 300) {
+                    $lvlBtn.css("background-color", "green");
+                };
+            });
+    
+
+
+        // Levels Up the Player if Button is Clicked and Money is Sufficient 
+            $lvlBtn.on('click', function(e) {
+                e.preventDefault();
+
+                if ($money === 300) {
+                    $lvl += 1;
+                    $money -= 300;
+                    console.log($lvl);
+                };
+
+            });
+
+
+
+    });
+
+    // Selects Grid Squares at Random and Appends Icon if They're Empty
+    function randomId() {
+
+        var $rannum = Math.floor(Math.random() * 17);
+        var $id = $('#square' + $rannum);
+
+        if (  $id.contents().length == 0 ) {
+            $id.append($icon);
+        } else {
+
+        };
+    };
+
+    
+
+    
+
+});
